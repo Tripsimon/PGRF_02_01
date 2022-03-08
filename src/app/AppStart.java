000ef735a006ddbb9c50febd979da43621ea5f3a
Martin Lešák <tripsimon3@gmail.com>
2022-03-09 00:03:47 +0100

    Merge

--- src/app/AppStart.java
+++ src/app/AppStart.java
@@ -7,7 +7,7 @@ import javax.swing.*;
 
 public class AppStart {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { //KEK
         SwingUtilities.invokeLater(() -> {
             Window window = new Window();
             new Controller3D(window.getPanel());
